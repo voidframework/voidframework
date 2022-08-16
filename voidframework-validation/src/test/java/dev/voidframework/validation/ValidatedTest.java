7cf3b75b9ba41a96ac92a91f04709435b7126069
Thibault Meyer <meyer.thibault@gmail.com>
2022-08-16 20:42:58 +0200

    Validation constraints can be grouped (#30)
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

---
+++ voidframework-validation/src/test/java/dev/voidframework/validation/ValidatedTest.java
@@ -0,0 +1,248 @@
+package dev.voidframework.validation;
+
+import dev.voidframework.core.helper.Reflection;
+import jakarta.validation.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.List;
+import java.util.Map;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class ValidatedTest {
+
+    @Test
+    public void emptyOf() {
+
+        // Act
+        final Validated<Object> validated = Validated.emptyOf();
+
+        // Asset
+        Assertions.assertNotNull(validated);
+
+        final Object objInstance = Reflection.getFieldValue(validated, "instance", Object.class);
+        final Map<String, List<ValidationError>> validationErrorPerPathMap = Reflection.getFieldValue(
+            validated,
+            "validationErrorPerPathMap",
+            new Reflection.WrappedClass<>());
+
+        Assertions.assertNull(objInstance);
+        Assertions.assertNotNull(validationErrorPerPathMap);
+        Assertions.assertTrue(validationErrorPerPathMap.isEmpty());
+    }
+
+    @Test
+    public void emptyOfWithEntity() {
+
+        // Arrange
+        final SimpleEntity entity = new SimpleEntity("a@a");
+
+        // Act
+        final Validated<Object> validated = Validated.emptyOf(entity);
+
+        // Asset
+        Assertions.assertNotNull(validated);
+
+        final Object objInstance = Reflection.getFieldValue(validated, "instance", Object.class);
+        Assertions.assertNotNull(objInstance);
+        Assertions.assertTrue(objInstance instanceof SimpleEntity);
+
+        final String email = Reflection.getFieldValue(objInstance, "email", String.class);
+        Assertions.assertEquals("a@a", email);
+
+        final Map<String, List<ValidationError>> validationErrorPerPathMap = Reflection.getFieldValue(
+            validated,
+            "validationErrorPerPathMap",
+            new Reflection.WrappedClass<>());
+        Assertions.assertNotNull(validationErrorPerPathMap);
+        Assertions.assertTrue(validationErrorPerPathMap.isEmpty());
+    }
+
+    @Test
+    public void hasError() {
+
+        // Arrange
+        final List<ValidationError> validationErrorList = List.of(new ValidationError("message", "messageKey"));
+        final Map<String, List<ValidationError>> validationErrorPerPathMap = Map.of("fieldName", validationErrorList);
+        final Validated<Object> validated = new Validated<>(null, validationErrorPerPathMap);
+
+        // Act
+        final boolean hasError = validated.hasError();
+
+        // Asset
+        Assertions.assertTrue(hasError);
+    }
+
+    @Test
+    public void hasErrorNoErrors() {
+
+        // Arrange
+        final Map<String, List<ValidationError>> validationErrorPerPathMap = Map.of();
+        final Validated<Object> validated = new Validated<>(null, validationErrorPerPathMap);
+
+        // Act
+        final boolean hasError = validated.hasError();
+
+        // Asset
+        Assertions.assertFalse(hasError);
+    }
+
+    @Test
+    public void hasErrorSpecificPath() {
+
+        // Arrange
+        final List<ValidationError> validationErrorList = List.of(new ValidationError("message", "messageKey"));
+        final Map<String, List<ValidationError>> validationErrorPerPathMap = Map.of("fieldName", validationErrorList);
+        final Validated<Object> validated = new Validated<>(null, validationErrorPerPathMap);
+
+        // Act
+        final boolean hasError = validated.hasError("fieldName");
+
+        // Asset
+        Assertions.assertTrue(hasError);
+    }
+
+    @Test
+    public void hasErrorSpecificPathNoErrors() {
+
+        // Arrange
+        final List<ValidationError> validationErrorList = List.of(new ValidationError("message", "messageKey"));
+        final Map<String, List<ValidationError>> validationErrorPerPathMap = Map.of("fieldName", validationErrorList);
+        final Validated<Object> validated = new Validated<>(null, validationErrorPerPathMap);
+
+        // Act
+        final boolean hasError = validated.hasError("fieldName2");
+
+        // Asset
+        Assertions.assertFalse(hasError);
+    }
+
+    @Test
+    public void isValid() {
+
+        // Arrange
+        final Map<String, List<ValidationError>> validationErrorPerPathMap = Map.of();
+        final Validated<Object> validated = new Validated<>(null, validationErrorPerPathMap);
+
+        // Act
+        final boolean isValid = validated.isValid();
+
+        // Asset
+        Assertions.assertTrue(isValid);
+    }
+
+    @Test
+    public void isValidNotValid() {
+
+        // Arrange
+        final List<ValidationError> validationErrorList = List.of(new ValidationError("message", "messageKey"));
+        final Map<String, List<ValidationError>> validationErrorPerPathMap = Map.of("fieldName", validationErrorList);
+        final Validated<Object> validated = new Validated<>(null, validationErrorPerPathMap);
+
+        // Act
+        final boolean isValid = validated.isValid();
+
+        // Asset
+        Assertions.assertFalse(isValid);
+    }
+
+    @Test
+    public void getInstance() {
+
+        // Arrange
+        final SimpleEntity entity = new SimpleEntity("name@domain.local");
+        final Map<String, List<ValidationError>> validationErrorPerPathMap = Map.of();
+        final Validated<SimpleEntity> validated = new Validated<>(entity, validationErrorPerPathMap);
+
+        // Act
+        final SimpleEntity entityInstance = validated.getInstance();
+
+        // Asset
+        Assertions.assertNotNull(entityInstance);
+        Assertions.assertEquals("name@domain.local", entityInstance.email);
+    }
+
+    @Test
+    public void getError() {
+
+        // Arrange
+        final List<ValidationError> validationErrorList = List.of(new ValidationError("message", "messageKey"));
+        final Map<String, List<ValidationError>> validationErrorPerPathMap = Map.of("fieldName", validationErrorList);
+        final Validated<Object> validated = new Validated<>(null, validationErrorPerPathMap);
+
+        // Act
+        final Map<String, List<ValidationError>> validationErrorMap = validated.getError();
+
+        // Asset
+        Assertions.assertNotNull(validationErrorMap);
+        Assertions.assertEquals(1, validationErrorMap.size());
+        Assertions.assertTrue(validationErrorMap.containsKey("fieldName"));
+
+        final List<ValidationError> validationErrorListActual = validationErrorMap.get("fieldName");
+        Assertions.assertEquals(1, validationErrorListActual.size());
+        Assertions.assertEquals("message", validationErrorListActual.get(0).getMessage());
+        Assertions.assertEquals("messageKey", validationErrorListActual.get(0).getMessageKey());
+    }
+
+    @Test
+    public void getErrorSpecificPath() {
+
+        // Arrange
+        final List<ValidationError> validationErrorList = List.of(new ValidationError("message", "messageKey"));
+        final Map<String, List<ValidationError>> validationErrorPerPathMap = Map.of("fieldName", validationErrorList);
+        final Validated<Object> validated = new Validated<>(null, validationErrorPerPathMap);
+
+        // Act
+        final ValidationError validationError = validated.getError("fieldName");
+
+        // Asset
+        Assertions.assertNotNull(validationError);
+        Assertions.assertEquals("message", validationError.getMessage());
+        Assertions.assertEquals("messageKey", validationError.getMessageKey());
+    }
+
+    @Test
+    public void getErrorList() {
+
+        // Arrange
+        final List<ValidationError> validationErrorList = List.of(new ValidationError("message", "messageKey"));
+        final Map<String, List<ValidationError>> validationErrorPerPathMap = Map.of("fieldName", validationErrorList);
+        final Validated<Object> validated = new Validated<>(null, validationErrorPerPathMap);
+
+        // Act
+        final List<ValidationError> validationErrorListActual = validated.getErrorList("fieldName");
+
+        // Asset
+        Assertions.assertNotNull(validationErrorListActual);
+        Assertions.assertEquals(1, validationErrorListActual.size());
+        Assertions.assertEquals("message", validationErrorListActual.get(0).getMessage());
+        Assertions.assertEquals("messageKey", validationErrorListActual.get(0).getMessageKey());
+    }
+
+    @Test
+    public void getErrorListNotFound() {
+
+        // Arrange
+        final List<ValidationError> validationErrorList = List.of(new ValidationError("message", "messageKey"));
+        final Map<String, List<ValidationError>> validationErrorPerPathMap = Map.of("fieldName", validationErrorList);
+        final Validated<Object> validated = new Validated<>(null, validationErrorPerPathMap);
+
+        // Act
+        final List<ValidationError> validationErrorListActual = validated.getErrorList("fieldName2");
+
+        // Asset
+        Assertions.assertNotNull(validationErrorListActual);
+        Assertions.assertEquals(0, validationErrorListActual.size());
+    }
+
+    /**
+     * Simple entity.
+     *
+     * @param email The email address
+     */
+    private record SimpleEntity(@Email @Length(min = 5, max = 10) String email) {
+    }
+}
--- voidframework-validation/src/test/java/dev/voidframework/validation/ValidationTest.java
+++ voidframework-validation/src/test/java/dev/voidframework/validation/ValidationTest.java
@@ -3,6 +3,7 @@ package dev.voidframework.validation;
 import jakarta.validation.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -15,63 +16,199 @@ import java.util.Locale;
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public final class ValidationTest {
 
+    @BeforeAll
+    public static void beforeAll() {
+
+        Locale.setDefault(Locale.ENGLISH);
+    }
+
     @Test
-    public void withError() {
+    public void validateNullValue() {
 
         // Arrange
-        final Pojo pojo = new Pojo("Camille Dominique");
         final Validation validation = new Validation();
 
         // Act
-        final Validated<Pojo> pojoValidated = validation.validate(pojo, Locale.ENGLISH);
+        final Validated<SimpleEntity> entityValidated = validation.validate(null, Locale.ENGLISH);
 
         // Assert
-        Assertions.assertNotNull(pojoValidated);
-        Assertions.assertTrue(pojoValidated.hasError());
-        Assertions.assertFalse(pojoValidated.isValid());
+        Assertions.assertNotNull(entityValidated);
+        Assertions.assertFalse(entityValidated.hasError());
+        Assertions.assertTrue(entityValidated.isValid());
+    }
 
-        final List<ValidationError> validationErrorList = pojoValidated.getErrorList("firstName");
+    @Test
+    public void validateNullValueWithoutLocale() {
+
+        // Arrange
+        final Validation validation = new Validation();
+
+        // Act
+        final Validated<SimpleEntity> entityValidated = validation.validate(null);
+
+        // Assert
+        Assertions.assertNotNull(entityValidated);
+        Assertions.assertFalse(entityValidated.hasError());
+        Assertions.assertTrue(entityValidated.isValid());
+    }
+
+    @Test
+    public void validateWithError() {
+
+        // Arrange
+        final SimpleEntityWithConstraintGroups entity = new SimpleEntityWithConstraintGroups("Camille", "aa");
+        final Validation validation = new Validation();
+
+        // Act
+        final Validated<SimpleEntityWithConstraintGroups> entityValidated = validation.validate(
+            entity,
+            Locale.ENGLISH,
+            GroupOne.class,
+            GroupTwo.class);
+
+        // Assert
+        Assertions.assertNotNull(entityValidated);
+        Assertions.assertTrue(entityValidated.hasError());
+        Assertions.assertFalse(entityValidated.isValid());
+
+        final List<ValidationError> validationErrorList = entityValidated.getErrorList("email");
         Assertions.assertNotNull(validationErrorList);
         Assertions.assertEquals(2, validationErrorList.size());
 
         final int idxLength = validationErrorList.get(0).getMessageKey().contains("Length.message") ? 0 : 1;
-        Assertions.assertEquals("length must be between 1 and 10", validationErrorList.get(idxLength).getMessage());
+        Assertions.assertEquals("length must be between 5 and 125", validationErrorList.get(idxLength).getMessage());
         Assertions.assertEquals("org.hibernate.validator.constraints.Length.message", validationErrorList.get(idxLength).getMessageKey());
         Assertions.assertEquals(2, validationErrorList.get(idxLength).getArgumentArray().length);
-        Assertions.assertEquals(1, validationErrorList.get(idxLength).getArgumentArray()[0]);
-        Assertions.assertEquals(10, validationErrorList.get(idxLength).getArgumentArray()[1]);
+        Assertions.assertEquals(5, validationErrorList.get(idxLength).getArgumentArray()[0]);
+        Assertions.assertEquals(125, validationErrorList.get(idxLength).getArgumentArray()[1]);
 
         final int idxEmail = validationErrorList.get(0).getMessageKey().contains("Email.message") ? 0 : 1;
         Assertions.assertEquals("must be a well-formed email address", validationErrorList.get(idxEmail).getMessage());
         Assertions.assertEquals("jakarta.validation.constraints.Email.message", validationErrorList.get(idxEmail).getMessageKey());
         Assertions.assertEquals(0, validationErrorList.get(idxEmail).getArgumentArray().length);
 
-        Assertions.assertEquals(pojo, pojoValidated.getInstance());
+        Assertions.assertEquals(entity, entityValidated.getInstance());
     }
 
     @Test
-    public void withoutError() {
+    public void validateWithErrorWithoutLocale() {
 
         // Arrange
-        final Pojo pojo = new Pojo("abc@local");
+        final SimpleEntityWithConstraintGroups entity = new SimpleEntityWithConstraintGroups("Camille", "aa");
         final Validation validation = new Validation();
 
         // Act
-        final Validated<Pojo> pojoValidated = validation.validate(pojo, Locale.ENGLISH);
+        final Validated<SimpleEntityWithConstraintGroups> entityValidated = validation.validate(
+            entity,
+            Locale.ENGLISH,
+            GroupOne.class,
+            GroupTwo.class);
 
         // Assert
-        Assertions.assertNotNull(pojoValidated);
-        Assertions.assertFalse(pojoValidated.hasError());
-        Assertions.assertTrue(pojoValidated.isValid());
+        Assertions.assertNotNull(entityValidated);
+        Assertions.assertTrue(entityValidated.hasError());
+        Assertions.assertFalse(entityValidated.isValid());
 
-        Assertions.assertEquals(pojo, pojoValidated.getInstance());
+        final List<ValidationError> validationErrorList = entityValidated.getErrorList("email");
+        Assertions.assertNotNull(validationErrorList);
+        Assertions.assertEquals(2, validationErrorList.size());
+
+        final int idxLength = validationErrorList.get(0).getMessageKey().contains("Length.message") ? 0 : 1;
+        Assertions.assertEquals("length must be between 5 and 125", validationErrorList.get(idxLength).getMessage());
+        Assertions.assertEquals("org.hibernate.validator.constraints.Length.message", validationErrorList.get(idxLength).getMessageKey());
+        Assertions.assertEquals(2, validationErrorList.get(idxLength).getArgumentArray().length);
+        Assertions.assertEquals(5, validationErrorList.get(idxLength).getArgumentArray()[0]);
+        Assertions.assertEquals(125, validationErrorList.get(idxLength).getArgumentArray()[1]);
+
+        final int idxEmail = validationErrorList.get(0).getMessageKey().contains("Email.message") ? 0 : 1;
+        Assertions.assertEquals("must be a well-formed email address", validationErrorList.get(idxEmail).getMessage());
+        Assertions.assertEquals("jakarta.validation.constraints.Email.message", validationErrorList.get(idxEmail).getMessageKey());
+        Assertions.assertEquals(0, validationErrorList.get(idxEmail).getArgumentArray().length);
+
+        Assertions.assertEquals(entity, entityValidated.getInstance());
+    }
+
+    @Test
+    public void validateWithoutError() {
+
+        // Arrange
+        final SimpleEntity simpleEntity = new SimpleEntity("abc@local");
+        final Validation validation = new Validation();
+
+        // Act
+        final Validated<SimpleEntity> entityValidated = validation.validate(simpleEntity, Locale.ENGLISH);
+
+        // Assert
+        Assertions.assertNotNull(entityValidated);
+        Assertions.assertFalse(entityValidated.hasError());
+        Assertions.assertTrue(entityValidated.isValid());
+
+        Assertions.assertEquals(simpleEntity, entityValidated.getInstance());
+    }
+
+    @Test
+    public void validateWithconstraintGroupWithGroupOne() {
+
+        // Arrange
+        final SimpleEntityWithConstraintGroups entity = new SimpleEntityWithConstraintGroups("Bob", "");
+        final Validation validation = new Validation();
+
+        // Act
+        final Validated<SimpleEntityWithConstraintGroups> entityValidated = validation.validate(entity, Locale.ENGLISH, GroupOne.class);
+
+        // Assert
+        Assertions.assertNotNull(entityValidated);
+        Assertions.assertFalse(entityValidated.hasError());
+        Assertions.assertTrue(entityValidated.isValid());
+
+        Assertions.assertEquals(entity, entityValidated.getInstance());
+    }
+
+    @Test
+    public void validateWithconstraintGroupWithGroupTwo() {
+
+        // Arrange
+        final SimpleEntityWithConstraintGroups entity = new SimpleEntityWithConstraintGroups("", "abc@domain.local");
+        final Validation validation = new Validation();
+
+        // Act
+        final Validated<SimpleEntityWithConstraintGroups> entityValidated = validation.validate(entity, Locale.ENGLISH, GroupTwo.class);
+
+        // Assert
+        Assertions.assertNotNull(entityValidated);
+        Assertions.assertFalse(entityValidated.hasError());
+        Assertions.assertTrue(entityValidated.isValid());
+
+        Assertions.assertEquals(entity, entityValidated.getInstance());
     }
 
     /**
-     * Pojo.
+     * Constraint group 1.
+     */
+    private interface GroupOne {
+    }
+
+    /**
+     * Constraint group 1.
+     */
+    private interface GroupTwo {
+    }
+
+    /**
+     * Simple entity.
      *
      * @param firstName The first name
      */
-    private record Pojo(@Email @Length(min = 1, max = 10) String firstName) {
+    private record SimpleEntity(@Length(min = 1, max = 10) String firstName) {
+    }
+
+    /**
+     * Simple entity with constraint groups.
+     *
+     * @param firstName The first name
+     * @param email     The email address
+     */
+    private record SimpleEntityWithConstraintGroups(@Length(min = 1, max = 10, groups = GroupOne.class) String firstName,
+                                                    @Email(groups = GroupTwo.class) @Length(min = 5, max = 125, groups = GroupTwo.class) String email) {
     }
 }
