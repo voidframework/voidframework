117743d28b3917e69cf6dd45703e93e963ad2efa
Thibault Meyer <meyer.thibault@gmail.com>
2023-04-07 08:43:16 +0200

    Add CachePut annotation (#102)
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

--- voidframework-cache/src/main/java/dev/voidframework/cache/module/CacheModule.java
+++ voidframework-cache/src/main/java/dev/voidframework/cache/module/CacheModule.java
@@ -2,6 +2,7 @@ package dev.voidframework.cache.module;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.matcher.Matchers;
+import dev.voidframework.cache.annotation.CachePut;
 import dev.voidframework.cache.annotation.CacheRemove;
 import dev.voidframework.cache.annotation.CacheResult;
 import dev.voidframework.cache.engine.CacheEngine;
@@ -16,12 +17,16 @@ public final class CacheModule extends AbstractModule {
     @Override
     protected void configure() {
 
+        final CacheInterceptor cacheInterceptorPut = new CacheInterceptorPut();
         final CacheInterceptor cacheInterceptorRemove = new CacheInterceptorRemove();
         final CacheInterceptor cacheInterceptorResult = new CacheInterceptorResult();
 
+        requestInjection(cacheInterceptorPut);
         requestInjection(cacheInterceptorRemove);
         requestInjection(cacheInterceptorResult);
+
         bind(CacheEngine.class).toProvider(CacheEngineProvider.class);
+        bindInterceptor(Matchers.any(), Matchers.annotatedWith(CachePut.class), cacheInterceptorPut);
         bindInterceptor(Matchers.any(), Matchers.annotatedWith(CacheRemove.class), cacheInterceptorRemove);
         bindInterceptor(Matchers.any(), Matchers.annotatedWith(CacheResult.class), cacheInterceptorResult);
     }
